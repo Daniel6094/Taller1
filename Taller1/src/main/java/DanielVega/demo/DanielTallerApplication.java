4cc1039b22ae5eef53de7a89a9140b302bb35e90
Daniel6094 <167911360+Daniel6094@users.noreply.github.com>
2024-10-22 19:56:43 -0600

    Dia1

---
+++ Taller1/src/main/java/DanielVega/demo/DanielTallerApplication.java
@@ -0,0 +1,13 @@
+package Taller1.src.main.java.DanielVega.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanielTallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanielTallerApplication.class, args);
+	}
+
+}
---
+++ Taller1/src/test/java/DanielVega/demo/DanielTallerApplicationTests.java
@@ -0,0 +1,13 @@
+package Taller1.src.test.java.DanielVega.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanielTallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
